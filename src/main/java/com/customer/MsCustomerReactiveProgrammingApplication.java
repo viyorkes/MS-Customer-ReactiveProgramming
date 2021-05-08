dfeb39c2dd5ec77d7e70050ae4c48e184193be8c
victor <victorsleiman25@gmail.com>
2021-05-08 01:04:56 -0300

    Creating project structure

---
+++ src/main/java/com/customer/MsCustomerReactiveProgrammingApplication.java
@@ -0,0 +1,15 @@
+package com.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MsCustomerReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerReactiveProgrammingApplication.class, args);
+	}
+
+}
