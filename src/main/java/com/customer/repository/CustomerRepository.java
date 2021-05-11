949de031ff1281e7b3e8eb3ae4567a6d4a3632bb
victor <victorsleiman25@gmail.com>
2021-05-11 20:02:50 -0300

    Creating class and test class customerRepository
    Test class with the methods (findAll, findById, updateCustomer and deleteCustomer)

--- src/main/java/com/customer/repository/CustomerRepository.java
+++ src/main/java/com/customer/repository/CustomerRepository.java
@@ -2,6 +2,12 @@ package com.customer.repository;
 
 import com.customer.document.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
+
+
+Mono<Customer> findByName(String name);
+
 }
--- src/test/java/com/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/com/customer/repository/CustomerRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -20,12 +21,12 @@ public class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
-    List < Customer > customerList = Arrays.asList(
+    List <Customer> customerList = Arrays.asList(
             new Customer(null, "jose", "12345678", 20),
-            new Customer(null, "victor", "12345679", 25),
+            new Customer(null, "victor", "99999999", 25),
             new Customer(null, "dani", "12345678", 20),
             new Customer(null, "leo", "12345679", 7),
-            new Customer(null, "jujuba", "12345679", 7)
+            new Customer("ABC", "jujuba", "12345679", 7)
     );
 
     @BeforeEach
@@ -35,18 +36,88 @@ public class CustomerRepositoryTest {
                 .thenMany(Flux.fromIterable(customerList))
                 .flatMap(customerRepository::save)
                 .doOnNext((customer -> {
-                    System.out.println("inserted Item:" + customer);
+                    System.out.println("inserted Customer:" + customer);
                 })).blockLast();
 
     }
 
     @Test
     public void getAllCustomers() {
-
         StepVerifier.create(customerRepository.findAll())
                 .expectSubscription()
                 .expectNextCount(5)
                 .verifyComplete();
+    }
+
+    @Test
+    public void getCustomerById() {
+        StepVerifier.create(customerRepository.findById("ABC"))
+                .expectSubscription()
+                .expectNextMatches((customer -> customer.getName().equals("jujuba")))
+                .verifyComplete();
+    }
+
+    @Test
+    public void findCustomerByName() {
+        StepVerifier.create(customerRepository.findByName("jujuba").log("findCustomerByName"))
+                .expectSubscription()
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void saveCustomer() {
+
+        Customer customer = new Customer("1", "Jose", "999999999", 26);
+
+        Mono <Customer> saveCustomer = customerRepository.save(customer);
+        StepVerifier.create(saveCustomer.log("saveCustomer"))
+                .expectSubscription()
+                .expectNextMatches(customer1 -> (customer1.getId() != null && customer1.getName().equals("Jose")))
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void updateCustomer() {
+
+        String newPhoneNumber = "8888888888";
+        Mono <Customer> updateCustomer = customerRepository.findByName("victor").log()
+                .map(customer -> {
+                    customer.setPhoneNumber(newPhoneNumber);
+                    return customer;
+                })
+                .flatMap(customer -> {
+                    return customerRepository.save(customer);
+                });
+
+        StepVerifier.create(updateCustomer.log("updated"))
+                .expectSubscription()
+                .expectNextMatches(customer -> customer.getPhoneNumber().equals("8888888888"))
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void deleteItemById() {
+
+        Mono <Void> deleteCustomer = customerRepository.findById("ABC")
+                .map(Customer::getId).log("get ID")
+                .flatMap((id) -> {
+                    return customerRepository.deleteById(id);
+                });
+
+        StepVerifier.create(deleteCustomer.log("ID deleted"))
+                .expectSubscription()
+                .verifyComplete();
+
+
+        StepVerifier.create(customerRepository.findAll().log("updated list"))
+                .expectSubscription()
+                .expectNextCount(4)
+                .verifyComplete();
+
     }
 
 }
